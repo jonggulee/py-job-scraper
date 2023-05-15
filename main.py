f25e3dc693624a314d9b4a36cce8c527b0ccb6ab
Jonggu Lee <jg.jake.lee@gmail.com>
2023-05-15 17:42:59 +0900

    css 추가

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from extractors.wwr import extract_wwr_jobs
 
 app = Flask("JobScrapper")
 
@@ -8,6 +9,9 @@ def home():
 
 @app.route("/search")
 def hello():
-    return render_template("search.html")
+    keyword = request.args.get("keyword")
+    wwr = extract_wwr_jobs(keyword)
+    jobs = wwr
+    return render_template("search.html", keyword=keyword, jobs=jobs)
  
 app.run("0.0.0.0", port=4000)
