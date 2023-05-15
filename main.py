c5e50d297392422226602cf30abd345463150058
Jonggu Lee <jg.jake.lee@gmail.com>
2023-05-15 09:27:12 +0900

    job scraper

---
+++ main.py
@@ -0,0 +1,14 @@
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
+
+keyword = input("What do you want to search for?")
+
+wwr = extract_wwr_jobs(keyword)
+# indeed = extract_indeed_jobs(keyword)
+# jobs = indeed + wwr
+
+jobs = wwr
+
+for job in jobs:
+  print(job)
+  print("")
