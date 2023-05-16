bb3e89c094d4470eb5ad2fa473a9a412802cc8b6
Jonggu Lee <jg.jake.lee@gmail.com>
2023-05-16 10:03:00 +0900

    Added file download

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 
 app = Flask("JobScrapper")
+db = {}
 
 @app.route("/")
 def home():
     return render_template("home.html", name="jake")
 
-db = {}
-
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
     if keyword in db:
         jobs = db[keyword]
     else:
@@ -20,4 +22,14 @@ def search():
         db[keyword] = jobs
     return render_template("search.html", keyword=keyword, jobs=jobs)
  
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
+
 app.run("0.0.0.0", port=4000)
